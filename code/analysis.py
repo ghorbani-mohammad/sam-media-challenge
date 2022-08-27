1df3427c4fbd299a315870c4ddb2fc25cee27148
Mohammad <aali361@gmail.com>
2022-08-27 16:05:26 +0430

    add pie chart and bar plot for users OS

--- code/analysis.py
+++ code/analysis.py
@@ -40,3 +40,45 @@ plt.title("Most Famous Affiliate")
 plt.pie(values, labels=keys, explode=[0, 0, 0.1], autopct="%1.1f%%")
 plt.xlabel("Nan values are ignored (near to zero)")
 plt.show()
+
+
+oses = set(users.os_name)
+oses_count = {}
+for os in oses:
+    count = len(users[users["os_name"] == os])
+    if count > 100:
+        oses_count[os] = count
+oses_count = {
+    k: v for k, v in sorted(oses_count.items(), key=lambda item: item[1])
+}
+
+keys = list(oses_count.keys())
+values = list(oses_count.values())
+
+plt.title("Most Famous OS")
+plt.pie(values, labels=keys, explode=[0, 0.1], autopct="%1.1f%%")
+plt.xlabel(
+    "Just considered OS that have at least 50 users. OS X, Windows Phone, iPadOS, HarmonyOS was ignored."
+)
+plt.show()
+
+
+oses_count = {}
+for os in oses:
+    count = len(users[users["os_name"] == os])
+    if count > 0:
+        oses_count[os] = count
+oses_count = {
+    k: v for k, v in sorted(oses_count.items(), key=lambda item: item[1])
+}
+
+keys = list(oses_count.keys())
+values = list(oses_count.values())
+
+plt.title("Most Famous OS")
+plt.bar(keys, values)
+for i in range(len(keys)):
+    plt.annotate(
+        str(values[i]), xy=(keys[i], values[i]), ha="center", va="bottom"
+    )
+plt.show()
