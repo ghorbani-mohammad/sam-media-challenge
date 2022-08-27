b4d0c0af412001e19d80fa61adeb865b6cd49539
Mohammad <aali361@gmail.com>
2022-08-27 20:53:30 +0430

    add UnSubscription-Count/Day bar chart

--- code/analysis.py
+++ code/analysis.py
@@ -121,7 +121,6 @@ delta = end_date - start_date
 for i in range(delta.days + 1):
     day = str(start_date + timedelta(days=i))
     subscriptions_date_count[day] = subscriptions_date_count.get(day, 0)
-subscriptions_date_count
 
 plt.bar(
     [key[5:] for key in subscriptions_date_count.keys()],
@@ -131,4 +130,30 @@ plt.xticks(rotation=90)
 plt.title("Subscription/Date")
 plt.xlabel("Date")
 plt.ylabel("Subscription Count")
+plt.show()
+
+
+unsubscriptions_date_count = {}
+for item in unsubscribed_users.subscription_date:
+    item = item.split()[0]
+    unsubscriptions_date_count[item] = (
+        unsubscriptions_date_count.get(item, 0) + 1
+    )
+
+start_date = date(2022, 6, 1)
+end_date = date(2022, 6, 30)
+delta = end_date - start_date
+
+for i in range(delta.days + 1):
+    day = str(start_date + timedelta(days=i))
+    unsubscriptions_date_count[day] = unsubscriptions_date_count.get(day, 0)
+
+plt.bar(
+    [key[5:] for key in unsubscriptions_date_count.keys()],
+    unsubscriptions_date_count.values(),
+)
+plt.xticks(rotation=90)
+plt.title("UnSubscription/Date")
+plt.xlabel("Date")
+plt.ylabel("UnSubscription Count")
 plt.show()
