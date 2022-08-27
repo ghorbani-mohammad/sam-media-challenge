893faae1345ffa4faca3cf5f4c9ba78d8e8e5b93
Mohammad <aali361@gmail.com>
2022-08-27 15:08:36 +0430

    add most active affiliate

--- code/analysis.py
+++ code/analysis.py
@@ -24,3 +24,19 @@ values = list(services_count.values())
 plt.title("Most Famous Service")
 plt.pie(values, labels=keys, explode=[0, 0, 0, 0, 0.1], autopct="%1.1f%%")
 plt.show()
+
+affiliates = set(users.affiliate)
+affiliates_count = {}
+for affiliate in affiliates:
+    affiliates_count[affiliate] = len(users[users["affiliate"] == affiliate])
+affiliates_count = {
+    k: v for k, v in sorted(affiliates_count.items(), key=lambda item: item[1])
+}
+
+keys = list(affiliates_count.keys())[1:]
+values = list(affiliates_count.values())[1:]
+
+plt.title("Most Famous Affiliate")
+plt.pie(values, labels=keys, explode=[0, 0, 0.1], autopct="%1.1f%%")
+plt.xlabel("Nan values are ignored (near to zero)")
+plt.show()
